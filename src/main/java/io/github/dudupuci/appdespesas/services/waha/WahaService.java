ffbfa12c7845ed1afa65bcda868ffef440e953a4
dudupuci <dudupucinelli@gmail.com>
2026-02-05 00:47:35 -0300

    :sparkles: mudando nome de var

--- src/main/java/io/github/dudupuci/appdespesas/services/waha/WahaService.java
+++ src/main/java/io/github/dudupuci/appdespesas/services/waha/WahaService.java
@@ -22,7 +22,7 @@ public class WahaService {
 
     private final RestTemplate restTemplate;
 
-    @Value("${waha.api.url:http://localhost:3001}")
+    @Value("${waha.webhook.url:http://localhost:3001}")
     private String wahaApiUrl;
 
     @Value("${waha.api.key:}")
